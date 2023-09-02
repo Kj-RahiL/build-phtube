1ac78e87c2a7c5e679ef21dcf9d5314381c8f479
Kj-RahiL <rahiilarham@gmail.com>
2023-09-02 20:51:43 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const loadPhTube = async () => {
     const categories = data.data
 
     displayPhTube(categories)
-    // console.log(arrObj[1])
+    // console.log(categories)
 
 
 }
 const displayPhTube = (categories) => {
     const tabContainer = document.getElementById('tab-container')
     categories.forEach(category => {
-        // console.log(category.category_id)
+        // console.log(category)
         const categoryTab = document.createElement('div')
         categoryTab.innerHTML = `
         <a onclick="handleTab('${category.category_id}')" class="tab bg-slate-200 mx-2 md:mx-4 lg:mx-4 mb-5">${category.category}</a>
@@ -21,21 +21,27 @@ const displayPhTube = (categories) => {
 
     // console.log(categories)
 }
-const handleTab = async (id) => {
 
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}
-    `)
+let item;
+const handleTab = async (id) => {
+item = id;
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await response.json()
     const cardContainer = document.getElementById('card-container')
     cardContainer.innerHTML = ""
+
     data.data.forEach(card => {
-        console.log(card)
+
+        const total = secMinHrs(card.others.posted_date);
+        // console.log(total)
+
         const cardDiv = document.createElement('div');
         cardDiv.innerHTML = `
              <div class="card bg-base-100">
                 <figure class=" h-40">
                 <img src="${card?.thumbnail}" alt="Shoes" />
-                <h2 id="time" class="absolute mt-28 ml-36 rounded-md bg-black text-white px-2">${card.others.posted_date}</h2>
+                <h2 class="absolute mt-32 ml-36 rounded-md bg-black text-white text-xs px-2">
+                ${card.others.posted_date ? total : ''}</h2>
                 </figure>
                  <div class="pt-4 flex flex-row items-center ">
                     <img class="w-12 h-12 rounded-full" src="${card?.authors[0]?.profile_picture}." alt="">
@@ -51,6 +57,7 @@ const handleTab = async (id) => {
             </div>
         `
         cardContainer.appendChild(cardDiv)
+
     })
     // console.log(data.data.length)
     noContent(data.data)
@@ -68,9 +75,33 @@ const noContent = (to) => {
 
 }
 
-const hrsMinSec = (){
-    const second
+const secMinHrs = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    // const second = seconds % 60;
+    const result = hours + 'hrs ' + minutes + 'min ' + 'ago';
+    // console.log(result)
+    return result;
 }
+
+// sort
+const sorting = async () => {
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${item}`)
+    const data = await response.json()
+    const sortData = data.data.sort((a, b)=>{
+    
+        let A = a.others.views.slice(0, -1);
+        let B = b.others.views.slice(0, -1);
+        console.log(B - A)
+        return B - A;
+  })
+  return sortData
+
+}
+
+
 
 loadPhTube()
 handleTab(1000)
+// sorting()

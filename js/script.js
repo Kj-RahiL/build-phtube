cbbb60fcd662cc7206dc5255c35f8ea51c088b71
Kj-RahiL <rahiilarham@gmail.com>
2023-09-02 07:41:15 +0600

    complete blog site and blue badge

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-const loadPhTube = async () =>{
+const loadPhTube = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
     const data = await res.json();
     const categories = data.data
-    
+
     displayPhTube(categories)
     // console.log(arrObj[1])
 
-    
+
 }
-const displayPhTube = (categories) =>{
+const displayPhTube = (categories) => {
     const tabContainer = document.getElementById('tab-container')
-    categories.forEach(category =>{
+    categories.forEach(category => {
         // console.log(category.category_id)
         const categoryTab = document.createElement('div')
         categoryTab.innerHTML = `
@@ -18,23 +18,25 @@ const displayPhTube = (categories) =>{
         `
         tabContainer.appendChild(categoryTab)
     })
-    
+
     // console.log(categories)
 }
-const handleTab = async (id)=>{
-    
+const handleTab = async (id) => {
+
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}
     `)
     const data = await response.json()
     const cardContainer = document.getElementById('card-container')
-    cardContainer.innerHTML=""
-    data.data.forEach(card=>{
+    cardContainer.innerHTML = ""
+    data.data.forEach(card => {
         console.log(card)
-        noContent(data)
         const cardDiv = document.createElement('div');
-        cardDiv.innerHTML=`
+        cardDiv.innerHTML = `
              <div class="card bg-base-100">
-                <figure class=" h-40"><img src="${card?.thumbnail}" alt="Shoes" /></figure>
+                <figure class=" h-40">
+                <img src="${card?.thumbnail}" alt="Shoes" />
+                <h2 id="time" class="absolute mt-28 ml-36 rounded-md bg-black text-white px-2">${card.others.posted_date}</h2>
+                </figure>
                  <div class="pt-4 flex flex-row items-center ">
                     <img class="w-12 h-12 rounded-full" src="${card?.authors[0]?.profile_picture}." alt="">
                     <h2 class="font-bold pl-2">${card?.title}</h2>
@@ -42,22 +44,32 @@ const handleTab = async (id)=>{
                  <div class="text-[#171717B3]">
                     <div class="flex pl-12 items-center">
                         <h2 class="pr-3">${card?.authors[0]?.profile_name}</h2>
-                        
-                        </div>
-                        <h3 class="pl-12 mb-4">Total views: ${card?.others?.views}</h3>
+                        <img src="${card.authors[0].verified === true ? './logo/blue-tick.svg' : ''}">  
                     </div>
-                  </div>
+                        <h3 class="pl-12 mb-4">Total views: ${card.others.views ? card.others.views : 'no views'}</h3>
+                </div>
+            </div>
         `
         cardContainer.appendChild(cardDiv)
     })
-    // noContent(false)
-    // console.log('rahil')
-    // console.log(data.data)
+    // console.log(data.data.length)
+    noContent(data.data)
 }
-const noContent = (card) =>{
-    console.log(card.data)
+const noContent = (to) => {
+    // console.log(to.length)
     const NoContentHere = document.getElementById('no-content')
-    
+    if (0 === to.length) {
+        NoContentHere.classList.remove('hidden')
+    }
+    else {
+        NoContentHere.classList.add('hidden')
+    }
+
+
+}
+
+const hrsMinSec = (){
+    const second
 }
 
 loadPhTube()

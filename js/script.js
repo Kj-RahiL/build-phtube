a7276a93f066147bf105cdcfd9c87b028209197a
Kj-RahiL <rahiilarham@gmail.com>
2023-09-02 23:07:23 +0600

    sorting and complete assignment

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ const displayPhTube = (categories) => {
         // console.log(category)
         const categoryTab = document.createElement('div')
         categoryTab.innerHTML = `
-        <a onclick="handleTab('${category.category_id}')" class="tab bg-slate-200 mx-2 md:mx-4 lg:mx-4 mb-5">${category.category}</a>
+        <a onclick="handleTab('${category.category_id}')" class="tab px-4 bg-slate-200 mx-2 md:mx-4 lg:mx-4 mb-5 hover:bg-[#FF1F3D] hover:text-white">${category.category}</a>
         `
         tabContainer.appendChild(categoryTab)
     })
@@ -24,13 +24,26 @@ const displayPhTube = (categories) => {
 
 let item;
 const handleTab = async (id) => {
-item = id;
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
-    const data = await response.json()
+    item = id;
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data = await response.json();
+    visibleData(data.data)
+}
+
+function visibleData(value){
+    const NoContentHere = document.getElementById('no-content')
+    if (0 === value.length) {
+        NoContentHere.classList.remove('hidden')
+    }
+    else {
+        NoContentHere.classList.add('hidden')
+    }
+
     const cardContainer = document.getElementById('card-container')
+
     cardContainer.innerHTML = ""
 
-    data.data.forEach(card => {
+    value.forEach(card => {
 
         const total = secMinHrs(card.others.posted_date);
         // console.log(total)
@@ -59,21 +72,21 @@ item = id;
         cardContainer.appendChild(cardDiv)
 
     })
-    // console.log(data.data.length)
-    noContent(data.data)
+    
 }
-const noContent = (to) => {
-    // console.log(to.length)
-    const NoContentHere = document.getElementById('no-content')
-    if (0 === to.length) {
-        NoContentHere.classList.remove('hidden')
-    }
-    else {
-        NoContentHere.classList.add('hidden')
-    }
+
+// const noContent = (to) => {
+//     // console.log(to.length)
+//     const NoContentHere = document.getElementById('no-content')
+//     if (0 === to.length) {
+//         NoContentHere.classList.remove('hidden')
+//     }
+//     else {
+//         NoContentHere.classList.add('hidden')
+//     }
 
 
-}
+// }
 
 const secMinHrs = (seconds) => {
     const hours = Math.floor(seconds / 3600);
@@ -90,13 +103,12 @@ const sorting = async () => {
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${item}`)
     const data = await response.json()
     const sortData = data.data.sort((a, b)=>{
-    
-        let A = a.others.views.slice(0, -1);
-        let B = b.others.views.slice(0, -1);
+        let A = parseInt(a.others.views)
+        let B = parseInt(b.others.views)
         console.log(B - A)
         return B - A;
-  })
-  return sortData
+  });
+  visibleData(sortData)
 
 }
 
